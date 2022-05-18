106e4f0ca4e577a5e617562c4c22e6a3cff47090
Budaházi Bence <budahazi.bence@petrik.hu>
2022-05-18 09:46:31 +0200

    Initial commit

---
+++ src/main/java/Eloadas.java
@@ -0,0 +1,7 @@
+public class Eloadas {
+    boolean [][] foglalasok;
+
+    public Eloadas(int sorokSzama, int helyekSzama) {
+        this.foglalasok = new boolean[sorokSzama][helyekSzama];
+    }
+}
---
+++ src/test/java/EloadasTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EloadasTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void pass(){
+        assertTrue(true);
+    }
+}
